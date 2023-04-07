1fd41c7f84fda26ecc3233823619a28bce4febaa
MohhammedAneesP <mohammedanees454@gmail.com>
2023-04-07 21:19:24 +0530

    committ

--- lib/application/downloads/downloads_bloc.dart
+++ lib/application/downloads/downloads_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 import 'package:netflix_clone/domain/downloads/i_downloads_repo.dart';
 
 import '../../domain/core/failures/main_failure.dart';
@@ -9,9 +10,15 @@ part 'downloads_event.dart';
 part 'downloads_state.dart';
 part 'downloads_bloc.freezed.dart';
 
+
+@injectable
 class DownloadsBloc extends Bloc<DownloadsEvent, DownloadsState> {
+  
+  
   final IDownloadsRepo _downloadsRepo;
   DownloadsBloc(this._downloadsRepo) : super(DownloadsState.inital()) {
+
+
     on<_GetDownloadsImages>((event, emit) async {
       emit(
         state.copyWith(
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:netflix_clone/application/downloads/downloads_bloc.dart';
 import 'package:netflix_clone/core/colors.dart';
 import 'package:netflix_clone/presentation/main_page/screen_main_page.dart';
 
 import 'domain/core/di/injectible.dart';
 
 Future<void> main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
   await configureInjection();
   runApp(const MyApp());
 }
@@ -15,20 +18,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "NetFlixDemo",
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent),
-          backgroundColor: Colors.black,
-          scaffoldBackgroundColor: backgroundColor,
-          fontFamily: GoogleFonts.montserrat().fontFamily,
-          textTheme: const TextTheme(
-            bodyText1: TextStyle(color: Colors.white),
-            bodyText2: TextStyle(color: Colors.white),
-          )),
-      home: const ScreenMianPage(),
-      debugShowCheckedModeBanner: false,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => getIt<DownloadsBloc>()),
+      ],
+      child: MaterialApp(
+        title: "NetFlixDemo",
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent),
+            backgroundColor: Colors.black,
+            scaffoldBackgroundColor: backgroundColor,
+            fontFamily: GoogleFonts.montserrat().fontFamily,
+            textTheme: const TextTheme(
+              bodyText1: TextStyle(color: Colors.white),
+              bodyText2: TextStyle(color: Colors.white),
+            )),
+        home: const ScreenMianPage(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
