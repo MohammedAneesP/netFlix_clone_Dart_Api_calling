53503f03601cb4a705538bc69adc573464ae9c62
MohhammedAneesP <mohammedanees454@gmail.com>
2023-04-07 20:54:39 +0530

    com

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:netflix_clone/core/colors.dart';
 import 'package:netflix_clone/presentation/main_page/screen_main_page.dart';
 
-void main(List<String> args) {
+import 'domain/core/di/injectible.dart';
+
+Future<void> main(List<String> args) async {
+  await configureInjection();
   runApp(const MyApp());
 }
 
@@ -16,16 +18,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "NetFlixDemo",
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        appBarTheme:const AppBarTheme(backgroundColor: Colors.transparent),
-        backgroundColor: Colors.black,
-        scaffoldBackgroundColor: backgroundColor,
-        fontFamily: GoogleFonts.montserrat().fontFamily,
-        textTheme: const TextTheme(
-          bodyText1: TextStyle(color: Colors.white),
-          bodyText2: TextStyle(color: Colors.white),
-        )
-      ),
+          primarySwatch: Colors.blue,
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent),
+          backgroundColor: Colors.black,
+          scaffoldBackgroundColor: backgroundColor,
+          fontFamily: GoogleFonts.montserrat().fontFamily,
+          textTheme: const TextTheme(
+            bodyText1: TextStyle(color: Colors.white),
+            bodyText2: TextStyle(color: Colors.white),
+          )),
       home: const ScreenMianPage(),
       debugShowCheckedModeBanner: false,
     );
